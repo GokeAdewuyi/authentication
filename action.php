c33056795811d9aa4d4d9c06ec68c8755382ed87
GokeAdewuyi <adewuyiyusuf@yahoo.com>
2021-03-06 14:30:11 +0100

    update

--- action.php
+++ action.php
@@ -9,7 +9,7 @@ if (filter_has_var(INPUT_POST, 'register')) {
     }
     if (array_key_exists($_REQUEST['email'], $_SESSION)) {
         echo "Email already exist";
-        echo "&nbsp;&nbsp;&nbsp;&nbsp;<a href='register.html'>Go Back</a>";
+        echo "&nbsp;&nbsp;&nbsp;&nbsp;<a href='register.html' style='float: right; text-decoration: none; background-color: #b10202; color: white; padding: 14px 20px; margin: 8px 0; border: none; border-radius: 4px; cursor: pointer;'>Go Back</a>";
         return;
     }
     $_SESSION[$_REQUEST['email']] = $_REQUEST;
@@ -20,14 +20,14 @@ if (filter_has_var(INPUT_POST, 'register')) {
 if (filter_has_var(INPUT_POST, 'login')) {
     if (array_key_exists($_REQUEST['email'], $_SESSION)) {
         if ($_REQUEST['email'] === $_SESSION[$_REQUEST['email']]['email'] && $_REQUEST['password'] === $_SESSION[$_REQUEST['email']]['password']) {
-            echo "User is logged in";
-            echo "&nbsp;&nbsp;&nbsp;&nbsp;<a href='login.html'>Logout</a>";
+            echo "<span>Welcome ".$_REQUEST['email']."</span>";
+            echo "&nbsp;&nbsp;&nbsp;&nbsp;<a href='login.html' style='float: right; text-decoration: none; background-color: #b10202; color: white; padding: 14px 20px; margin: 8px 0; border: none; border-radius: 4px; cursor: pointer;'>Logout</a>";
         } else {
             echo "Invalid email address or password";
-            echo "&nbsp;&nbsp;&nbsp;&nbsp;<a href='login.html'>Go Back</a>";
+            echo "&nbsp;&nbsp;&nbsp;&nbsp;<a href='login.html' style='float: right; text-decoration: none; background-color: #b10202; color: white; padding: 14px 20px; margin: 8px 0; border: none; border-radius: 4px; cursor: pointer;'>Go Back</a>";
         }
     } else {
         echo "Invalid email address or password";
-        echo "&nbsp;&nbsp;&nbsp;&nbsp;<a href='login.html'>Go Back</a>";
+        echo "&nbsp;&nbsp;&nbsp;&nbsp;<a href='login.html' style='float: right; text-decoration: none; background-color: #b10202; color: white; padding: 14px 20px; margin: 8px 0; border: none; border-radius: 4px; cursor: pointer;'>Go Back</a>";
     }
 };
